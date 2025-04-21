c108c3edb0be564ffef6554e20b34a6d17ef3ab1
Venkatesh <vengi2k5@gmail.com>
2025-04-21 12:11:03 +0530

    Add newline charactor to the delimeter list

--- test/test_input_processor.rb
+++ test/test_input_processor.rb
@@ -4,10 +4,20 @@ class InputProcessorTest < Minitest::Test
  
   def setup
   end
+
   def test_parse_data_with_valid_values_comma_seperated
     expected = [2,4]
     input = "2,4"
     delimeter = [","]
+    actual = InputProcessor.parse_data(input,delimeter)    
+    assert_equal expected, actual
+  end
+
+  def test_parse_data_with_valid_value_comman_and_newline
+    expected = [2,4,5]
+    input = "2,4\n5"
+    delimeter = [",","\n"]
+    delimeter = delimeter.each{|d| Regexp.escape(d)}
     actual = InputProcessor.parse_data(input,delimeter)    
     assert_equal expected, actual
   end
